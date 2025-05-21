76481c435364cdb51f6d75de4e14390206cf7aab
Ashhiii <delapena.jefferson11@gmail.com>
2025-05-21 18:57:46 +0800

    first

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+# yourapp/models.py
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db import models
 
@@ -16,10 +17,6 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, email, username, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
         return self.create_user(email, username, password, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
 from .views import RegisterView, LoginView
-from .views import VerifyEmailView
-
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('verify-email/<uidb64>/<token>/', VerifyEmailView.as_view(), name='verify-email'),
- 
+     path('login/', LoginView.as_view(), name='login'), 
 ]
